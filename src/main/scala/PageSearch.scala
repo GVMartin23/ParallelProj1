526f543a48314c05c0bfdf741ff75b24fe68cd50
noYuen <yuennr20@gcc.edu>
2024-04-11 15:21:10 -0400

    tailrec stuff

--- src/main/scala/PageSearch.scala
+++ src/main/scala/PageSearch.scala
@@ -1,6 +1,6 @@
-import scala.annotation.tailrec
 import scala.math.log
-import scala.collection.parallel.CollectionConverters.*
+import scala.collection.parallel.CollectionConverters._
+import scala.annotation.tailrec
 
 object PageSearch {
     /**
@@ -9,11 +9,20 @@ object PageSearch {
      * @return       a list of the number of times any of the terms appeared in each page in the same order as given
      */
     def count(pages: List[RankedWebPage], query: List[String]): List[Double] = {
+        List() // TODO: implement this method and remove this stub
         pages.map(p =>
             numInPages(p, query)
             )
     }
 
+
+  /**
+   * tail recursive counting for number of times any word in the query appears in the page
+   * @param p the current page
+   * @param query list of words that were queried
+   * @param countSoFar count of appearances
+   * @return
+   */
     @tailrec
     private def numInPages(p: RankedWebPage, query: List[String], occurrences: Double = 0.0): Double = query match
         case Nil => occurrences
